5514e9ca80f2b190978a8ab82925794045a74211
DangubaGitHub <dangubicjovan@outlook.com>
2022-03-10 22:31:10 +0100

    Added Bullet script and particle effects
     but its not working

---
+++ Ukraine Defender V_2/Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] ParticleSystem metalHit;
+    [SerializeField] ParticleSystem woodHit;
+    [SerializeField] ParticleSystem concreteHit;
+    [SerializeField] ParticleSystem bodyHit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Concrete"))
+        {
+            concreteHit = Instantiate(concreteHit, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if(collision.gameObject.CompareTag("Wood"))
+        {
+            woodHit = Instantiate(woodHit, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Metal"))
+        {
+            metalHit = Instantiate(metalHit, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+        if(collision.gameObject.CompareTag("Body"))
+        {
+            bodyHit = Instantiate(bodyHit, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
