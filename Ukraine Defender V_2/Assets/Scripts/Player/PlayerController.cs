e7f4169916183d6ea892ef0e2c7920c08c253ed4
DangubaGitHub <dangubicjovan@outlook.com>
2022-03-10 14:00:26 +0100

    added player mouse rotation

--- Ukraine Defender V_2/Assets/Scripts/Player/PlayerController.cs
+++ Ukraine Defender V_2/Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed;
     Rigidbody2D playerRb;
     Vector2 movement;
+    Vector2 mousePos;
+    [SerializeField] Camera cam;
 
     void Start()
     {
@@ -17,10 +19,15 @@ public class PlayerController : MonoBehaviour
     {
         movement.x = Input.GetAxis("Horizontal");
         movement.y = Input.GetAxis("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         playerRb.MovePosition(playerRb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - playerRb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        playerRb.rotation = angle;
     }
 }
