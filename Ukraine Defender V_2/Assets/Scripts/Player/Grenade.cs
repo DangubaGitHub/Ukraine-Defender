84aa092cb39776b4a35bbef48d735ab8eb481999
DangubaGitHub <dangubicjovan@outlook.com>
2022-03-12 17:24:57 +0100

    added grenade throw mechanics

--- Ukraine Defender V_2/Assets/Scripts/Player/Grenade.cs
+++ Ukraine Defender V_2/Assets/Scripts/Player/Grenade.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-
+    [SerializeField] GameObject grenadePrefab;
     [SerializeField] Animator grenadeAnim;
+    [SerializeField] Transform throwPoint;
+    [SerializeField] float throwForce;
 
     void Start()
     {
@@ -16,8 +18,18 @@ public class Grenade : MonoBehaviour
     {
         if(Input.GetButtonDown("Fire2"))
         {
+            Invoke ("Throw", .5f);
+
             grenadeAnim.ResetTrigger("Throw");
             grenadeAnim.SetTrigger("Throw");
         }
+    }
+
+    void Throw()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, throwPoint.position, throwPoint.rotation);
+        Rigidbody2D grenadeRb = grenade.GetComponent<Rigidbody2D>();
+        grenadeRb.AddForce(throwPoint.up * throwForce, ForceMode2D.Impulse);
+
     }
 }
