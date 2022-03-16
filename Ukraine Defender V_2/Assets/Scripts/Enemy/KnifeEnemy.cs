9c92d68ad73ef00a1e2514fa38add6b39cb0fbd5
DangubaGitHub <dangubicjovan@outlook.com>
2022-03-16 13:23:48 +0100

    Knife enemy finished

--- Ukraine Defender V_2/Assets/Scripts/Enemy/KnifeEnemy.cs
+++ Ukraine Defender V_2/Assets/Scripts/Enemy/KnifeEnemy.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class KnifeEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform target;
+    [SerializeField] float minDistance;
+    [SerializeField] float aggroDistance;
+    [SerializeField] Animator knifeSlash;
+    Rigidbody2D enemyRb;
+
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) < aggroDistance)
+        {
+            Vector2 lookDir = target.position - transform.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+            enemyRb.rotation = angle;
+
+            if (Vector2.Distance(transform.position, target.position) > minDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            }
+
+            if (Vector2.Distance(transform.position, target.position) <= minDistance)
+            {
+                knifeSlash.ResetTrigger("Slash");
+                knifeSlash.SetTrigger("Slash");
+            }
+        }
     }
 }
