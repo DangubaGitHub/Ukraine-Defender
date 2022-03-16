910d118770b7e67b51aa52499d65f62818e45376
DangubaGitHub <dangubicjovan@outlook.com>
2022-03-16 19:04:58 +0100

    pipe enemy finished

--- Ukraine Defender V_2/Assets/Scripts/Enemy/PipeEnemy.cs
+++ Ukraine Defender V_2/Assets/Scripts/Enemy/PipeEnemy.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PipeEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] Transform target;
+    [SerializeField] float minDistance;
+    [SerializeField] float aggroDistance;
+    [SerializeField] Animator pipeHit;
+    Rigidbody2D enemyRb;
+
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) < aggroDistance)
+        {
+            Vector2 lookDir = target.position - transform.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+            enemyRb.rotation = angle;
+
+            if (Vector2.Distance(transform.position, target.position) > minDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            }
+
+            if (Vector2.Distance(transform.position, target.position) <= minDistance)
+            {
+                pipeHit.ResetTrigger("Hit");
+                pipeHit.SetTrigger("Hit");
+            }
+        }
     }
 }
